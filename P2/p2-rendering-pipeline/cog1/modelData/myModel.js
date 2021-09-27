2d07a4e1b4825ac2a884ae3beba77aaef607d992
Jens Erhardt <j.erhardt@autohaus-royal.de>
2021-09-27 14:44:53 +0200

    Added vertices to add  doors.

--- P2/p2-rendering-pipeline/cog1/modelData/myModel.js
+++ P2/p2-rendering-pipeline/cog1/modelData/myModel.js
@@ -52,7 +52,15 @@ define(["exports", "data"], function (exports, data) {
       [-0.75, 3, 0], // 10
       [-0.75, 3, 1], // 11
       [-1, 1, 0], // 12
-      [-1, 1, 1], //13
+      [-1, 1, 1], // 13
+
+      [-0.5, -1, 1], //14
+      [0.5, -1, 1], // 15
+      [0.5, 0.25, 1], // 16
+      [-0.5, 0.25 , 1], // 17
+      
+      [0, -1, 1], // 18
+      [0, 0.25, 1] // 19
     ];
 
     instance.polygonVertices = [
@@ -63,9 +71,11 @@ define(["exports", "data"], function (exports, data) {
       [8, 9, 11, 10], // Tower side right
       [12, 13, 11, 10], // Tower left side
       [0, 3, 13, 12], // Side left
+      [14, 15 , 16, 17], // Door frame
+      [18, 19] // Door inner
     ];
 
-    instance.polygonColors = [0, 1, 2, 3, 4, 5, 6];
+    instance.polygonColors = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 
     data.applyScale.call(instance, scale);
 
